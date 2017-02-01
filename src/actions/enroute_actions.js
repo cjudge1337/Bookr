b576512ffa8069909e841f3e06ff3bcb8ce4d95b
Carson Judge <cjudge1337@gmail.com>
2017-02-01 10:53:39 -0800

    Create lyft and uber actions consts

--- src/actions/enroute_actions.js
+++ src/actions/enroute_actions.js
@@ -3,3 +3,30 @@ import * as UberAPIUtil from '../util/uber/quotes';
 export const RECEIVE_UBER_RIDE_INFO = "RECEIVE_UBER_RIDE_INFO";
 export const RECEIVE_UBER_MAP = "RECEIVE_UBER_MAP";
 export const REMOVE_UBER_RIDE = "REMOVE_UBER_RIDE";
+export const RECEIVE_LYFT_RIDE_INFO = "RECEIVE_LYFT_RIDE_INFO";
+export const REMOVE_LYFT_RIDE = "REMOVE_LYFT_RIDE";
+
+export const receieveUberRideInfo = info => ({
+  type: RECEIVE_UBER_RIDE_INFO,
+  info
+});
+
+export const receieveUberMap = mapInfo => ({
+  type: RECEIVE_UBER_RIDE_INFO,
+  mapInfo
+});
+
+export const removeUberRide = requestId => ({
+  type: REMOVE_UBER_RIDE,
+  requestId
+});
+
+export const receieveLyftRideInfo = info => ({
+  type: RECEIVE_LYFT_RIDE_INFO,
+  info
+});
+
+export const removeLyftRide = rideId => ({
+  type: REMOVE_LYFT_RIDE,
+  rideId
+});
