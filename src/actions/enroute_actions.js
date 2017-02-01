1f3323ac0d6d6d7319b0e2e62deb136bdd012800
Carson Judge <cjudge1337@gmail.com>
2017-02-01 10:23:14 -0800

    Create uber actions consts

---
+++ src/actions/enroute_actions.js
@@ -0,0 +1,5 @@
+import * as UberAPIUtil from '../util/uber/quotes';
+
+export const RECEIVE_UBER_RIDE_INFO = "RECEIVE_UBER_RIDE_INFO";
+export const RECEIVE_UBER_MAP = "RECEIVE_UBER_MAP";
+export const REMOVE_UBER_RIDE = "REMOVE_UBER_RIDE";
