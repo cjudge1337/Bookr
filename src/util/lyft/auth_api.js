5387478335a03346db6021ac8daca876791a166a
Carson Judge <cjudge1337@gmail.com>
2017-01-31 18:59:53 -0800

    Create #getUserProfiel lyft api util

---
+++ src/util/lyft/auth_api.js
@@ -0,0 +1,10 @@
+import $ from 'jQuery';
+import { LYFT_CLIENT_TOKEN, LYFT_CLIENT_SECRET } from '../../../config';
+
+export const getUserProfile = () => (
+  $.ajax({
+    url: "https://api.lyft.com/v1/profile",
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${LYFT_CLIENT_TOKEN}` }
+  })
+);
