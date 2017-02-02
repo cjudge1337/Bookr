2c1169e31d9b28775a2a816f1ce1da37fec218f1
Carson Judge <cjudge1337@gmail.com>
2017-02-01 21:21:51 -0800

    Structure TimeDisplay and EnrouteMap components

--- src/components/enroute/enroute_map.jsx
+++ src/components/enroute/enroute_map.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 class EnrouteMap extends React.Component {
   constructor(props) {
-
+    super(props);
   }
 
   render() {
-
+    return (
+      <div className="enroute-map-container">
+        Test map container
+      </div>
+    );
   }
 }
 
--- src/components/enroute/time_display.jsx
+++ src/components/enroute/time_display.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 class TimeDisplay extends React.Component {
   constructor(props) {
-
+    super(props);
   }
 
   render() {
-
+    return (
+      <div className="time-display-container">
+        <div className="pickup-container">
+          Pickup time
+        </div>
+        <div className="dropoff-container">
+          Dropoff time
+        </div>
+      </div>
+    );
   }
 }
 
