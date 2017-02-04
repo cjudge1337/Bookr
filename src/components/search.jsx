7eb9813b7facf6129077446c2aee025bc393a463
Peter Schrock <peterrschrock@gmail.com>
2017-02-04 15:37:17 -0800

    add vehicles, but failing

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -13,7 +13,7 @@ class Search extends React.Component {
   constructor(props){
     super(props);
 
-    bindAll(this, 'createETA', 'getUberTime','handleSelectDestination','renderOriginAutocomplete', 'renderDestinationAutocomplete', 'centsToDollars', 'renderResults', 'getUberResults','getLyftResults', 'getUserLocation');
+    bindAll(this, 'getCar', 'createETA', 'getUberTime','handleSelectDestination','renderOriginAutocomplete', 'renderDestinationAutocomplete', 'centsToDollars', 'renderResults', 'getUberResults','getLyftResults', 'getUserLocation');
 
 
   }
@@ -94,6 +94,23 @@ class Search extends React.Component {
       types={'address'}/>;
   }
 
+  getCar(name){
+    switch (name) {
+      case "POOL":
+        return <img className="uber-cars" src={require('../../app/images/uberX.png')}/>;
+      case "uberX":
+        return <img className="uber-cars" src={require('../../app/images/uberX.png')}/>;
+      case "BLACK":
+        return <img className="uber-cars" src={require('../../app/images/black.png')}/>;
+      case "uberXL":
+        return <img className="uber-cars" src={require('../../app/images/uberXL.png')}/>;
+      case "SUV":
+        return <img className="uber-cars" src={require('../../app/images/SUV.png')}/>;
+      default:
+
+    }
+  }
+
   getUberResults(){
     const that = this;
     console.log(that);
@@ -101,6 +118,7 @@ class Search extends React.Component {
 
       if(productObj.high_estimate > 0 && UBER_PRODUCTS.includes(productObj.display_name)){
         return (<li key={productObj.display_name} className="uber-lineitem">
+          {this.getCar(productObj.display_name)}
           <h3 className="uber-key-data">{productObj.display_name}</h3>
           <h3 className="uber-key-data">{productObj.estimate}</h3>
           <div className="uber-lineitem-times">
@@ -108,6 +126,7 @@ class Search extends React.Component {
               <h5>{that.getUberTime(productObj.display_name)} min</h5>
               <h5>away</h5>
             </div>
+
             <div className="time-inner-div">
               <h5>ETA:</h5>
               <h5>{this.createETA(that.getUberTime(productObj.display_name) + (productObj.duration / 60))}</h5>
@@ -124,9 +143,14 @@ class Search extends React.Component {
     let mins = now.getMinutes();
     let indicator;
     mins = (mins + rideLength) % 60;
+    if(mins < 10){
+      mins = `0${mins}`;
+    }
+
     if(now.getMinutes() + rideLength > 59){
       hrs = (hrs + 1) % 24;
     }
+
     if(hrs > 12){
       hrs -= 12;
       indicator = "PM";
@@ -174,13 +198,13 @@ class Search extends React.Component {
         <section className="results-container">
           <section className="uber-results">
             <div className="uber-header">
-              <img id="uber-logo" src={require('../../app/images/uber_rides_api_icon_2x_78px.png')}/>
+              <img id="uber-logo" src={require('../../app/images/uber_rides_api_icon_2x_50px.png')}/>
               <h1 className="company-titles">UBER</h1>
             </div>
             {this.getUberResults()}
           </section>
           <section className="lyft-results">
-            <img id="lyft-logo" src={require('../../app/images/lyft_header.png')}/>
+            <img id="lyft-logo" src={require('../../app/images/lyft_standard_silver.png')}/>
             {this.getLyftResults()}
           </section>
         </section>
