23dfc402e036bfb61adbb2d51674b59a75a77b21
Vinit <vinitp94@gmail.com>
2017-02-04 18:22:57 -0800

    Clearn up search container and open.jsx

--- src/components/search/search_container.js
+++ src/components/search/search_container.js
@@ -1,8 +1,8 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import Search from './search';
 import { clearPricesErrors, updateCurrentAddress, updateDestinationAddress,
   getDestinationGeolocation, getCurrentGeolocation, getLyftQuotes, getUberQuotes,
   getLyftETAs, getUberETAs } from '../../actions/quote_actions.js';
-import Search from './search';
 
 const mapStateToProps = store => ({
   quotes: store.quotes,
@@ -10,14 +10,21 @@ const mapStateToProps = store => ({
 
 const mapDispatchToProps = (dispatch, {location}) => (
   {
-    getLyftQuotes: (startLat, startLong, endLat, endLong) => dispatch(getLyftQuotes(startLat, startLong, endLat, endLong)),
-    getUberQuotes: (startLat, startLong, endLat, endLong) => dispatch(getUberQuotes(startLat, startLong, endLat, endLong)),
-    getLyftETAs: (latitude, longitude) => dispatch(getLyftETAs(latitude, longitude)),
-    getUberETAs: (latitude, longitude) => dispatch(getUberETAs(latitude, longitude)),
-    getDestinationGeolocation: (address) => dispatch(getDestinationGeolocation(address)),
-    getCurrentGeolocation: (address) => dispatch(getCurrentGeolocation(address)),
+    getLyftQuotes: (startLat, startLong, endLat, endLong) =>
+      dispatch(getLyftQuotes(startLat, startLong, endLat, endLong)),
+    getUberQuotes: (startLat, startLong, endLat, endLong) =>
+      dispatch(getUberQuotes(startLat, startLong, endLat, endLong)),
+    getLyftETAs: (latitude, longitude) =>
+      dispatch(getLyftETAs(latitude, longitude)),
+    getUberETAs: (latitude, longitude) =>
+      dispatch(getUberETAs(latitude, longitude)),
+    getDestinationGeolocation: (address) =>
+      dispatch(getDestinationGeolocation(address)),
+    getCurrentGeolocation: (address) =>
+      dispatch(getCurrentGeolocation(address)),
     updateCurrentAddress: address => dispatch(updateCurrentAddress(address)),
-    updateDestinationAddress: address => dispatch(updateDestinationAddress(address)),
+    updateDestinationAddress: address =>
+      dispatch(updateDestinationAddress(address)),
     clearStuff: () => dispatch(clearPricesErrors()),
   }
 );
