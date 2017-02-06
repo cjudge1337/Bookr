c5449281df664c7f933014135f20d199109385b0
Carson Judge <cjudge1337@gmail.com>
2017-02-05 17:26:42 -0800

    Add initial google map

--- src/components/enroute/enroute_map.jsx
+++ src/components/enroute/enroute_map.jsx
@@ -3,25 +3,28 @@ import React from 'react';
 class EnrouteMap extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      width: 250,
-      height: 250
-    };
   }
 
-  createMap() {
+  componentDidMount() {
+    this.map = new google.maps.Map(this.refs.map, {
+      center: {
+        lat: this.props.pickup.latitude,
+        lng: this.props.pickup.longitude
+      },
+      zoom: 13
+    });
 
+    let currCenter = this.map.getCenter();
+    google.maps.event.trigger(this.map, 'resize');
+    this.map.setCenter(currCenter);
   }
 
   render() {
     return (
       <div className="enroute-map-container">
-        <div id="map"></div>
-        map
+        <div ref="map" className="map"></div>
       </div>
     );
   }
 }
-
 export default EnrouteMap;
