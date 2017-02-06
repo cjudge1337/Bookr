be7e826a5a1c1642f4f44929e9df93c78f0238f5
Carson Judge <cjudge1337@gmail.com>
2017-02-05 17:55:03 -0800

    Able to make markers on map

--- src/components/enroute/enroute_map.jsx
+++ src/components/enroute/enroute_map.jsx
@@ -6,6 +6,21 @@ class EnrouteMap extends React.Component {
   }
 
   componentDidMount() {
+    let pickupGeo = {
+      lat: this.props.pickup.latitude,
+      lng: this.props.pickup.longitude
+    };
+
+    let driverGeo = {
+      lat: this.props.location.latitude,
+      lng: this.props.location.longitude
+    };
+
+    let destinationGeo = {
+      lat: this.props.destination.latitude,
+      lng: this.props.destination.longitude
+    };
+
     this.map = new google.maps.Map(this.refs.map, {
       center: {
         lat: this.props.pickup.latitude,
@@ -14,9 +29,27 @@ class EnrouteMap extends React.Component {
       zoom: 13
     });
 
-    let currCenter = this.map.getCenter();
-    google.maps.event.trigger(this.map, 'resize');
-    this.map.setCenter(currCenter);
+    const pickupMarker = new google.maps.Marker({
+      position: pickupGeo,
+      map: this.map,
+      title: 'Your location'
+    });
+
+    const driverMarker = new google.maps.Marker({
+      position: driverGeo,
+      map: this.map,
+      title: 'Driver location'
+    });
+
+    const destinationMarker = new google.maps.Marker({
+      position: destinationGeo,
+      map: this.map,
+      title: 'Destination location'
+    });
+// debugger
+    // let currCenter = this.map.getCenter();
+    // google.maps.event.trigger(this.map, 'resize');
+    // this.map.setCenter(currCenter);
   }
 
   render() {
